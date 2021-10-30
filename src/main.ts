ae41884b6d4f3daa8401a1c682cf14af2f37626c
Ivan Gorstkin <orlovsky.mr@gmail.com>
2021-10-31 00:33:45 +0300

    [OYC-25] chore: add env file and log server startup

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv/config';
+import { Logger } from '@nestjs/common';
+import { LogContexts } from './shared/models';
+
+const {
+  PORT
+} = process.env;
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  // tslint:disable-next-line:no-magic-numbers
-  await app.listen(3000);
+  await app.listen(PORT);
+
+  Logger.log(`Server started running on port ${PORT}`, LogContexts.SERVER);
 }
+
 bootstrap();
